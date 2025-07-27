d54725e7bf5b2f0f25d5070f888e6d0c5be25bf3
Natalia Zabolotnia <zanatav78@gmail.com>
2025-07-27 10:54:43 +0300

    initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,59 @@
+import CafeInfo from "../CafeInfo/CafeInfo";
+import css from "./App.module.css";
+import { useState } from "react";
+import type { Votes } from "../../types/votes";
+import VoteOptions from "../VoteOptions/VoteOptions";
+import VoteStats from "../VoteStats/VoteStats";
+import Notification from "../Notification/Notification";
+
+export default function App() {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+  const handleVote = (voteName: keyof Votes) => {
+    setVotes({
+      ...votes,
+      [voteName]: votes[voteName] + 1,
+    });
+  };
+
+  const resetVotes = () => {
+    setVotes({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  const canReset = votes.good > 0 || votes.neutral > 0 || votes.bad > 0;
+
+  const totalVotes = (votes: Votes): number => {
+    return votes.good + votes.neutral + votes.bad;
+  };
+  const total = totalVotes(votes);
+
+  const positiveRate = (votes: Votes): number => {
+    return total ? Math.round((votes.good / total) * 100) : 0;
+  };
+
+  const positive = positiveRate(votes);
+  return (
+    <div className={css.app}>
+      <CafeInfo />
+      {total > 0 ? (
+        <VoteStats votes={votes} total={total} positive={positive} />
+      ) : (
+        <div>
+          <Notification />
+        </div>
+      )}
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={canReset}
+      />
+    </div>
+  );
+}
